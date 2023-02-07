d27d3996b3edb36d27cb43bbc8e2d050246594c7
Keno <kenandi@kth.se>
2023-02-07 23:10:27 +0100

    FIxed byte error

--- task1/tcpclient/TCPClient.java
+++ task1/tcpclient/TCPClient.java
@@ -20,23 +20,17 @@ public class TCPClient {
      */
     public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
         
-            // Pre-allocate byte buffer
             byte[] fromServerBuffer = new byte[BUFFERSIZE];
-            
             int data;
 
-            // Open a connection to "hostname" at port with Socket()
             Socket clientSocket = new Socket(hostname, port);
-
-            // Send bytes via socket to server
             clientSocket.getOutputStream().write(toServerBytes);
 
-            // Set up ByteArrayOutputStream
             ByteArrayOutputStream dataFromServer = new ByteArrayOutputStream();
         
         try(InputStream inputStreamFromServer = clientSocket.getInputStream()) {
             while ((data = inputStreamFromServer.read(fromServerBuffer)) != -1) {
-                dataFromServer.write(fromServerBuffer);
+                dataFromServer.write(fromServerBuffer, 0, data);
                 fromServerBuffer = new byte[BUFFERSIZE];
             }
             clientSocket.close();
@@ -46,7 +40,14 @@ public class TCPClient {
             throw new IOException("Something went wrong: ", exception);
         }
     }
-
+    
+   /**
+    * Helper function for askServer()
+    * @param hostname server to establish a connection with
+    * @param port which port to use for connection
+    * @return byte[] with data recieved from server
+    * @throws IOException
+    */
     public byte[] askServer(String hostname, int port) throws IOException {
         return askServer(hostname, port, null);
     }
