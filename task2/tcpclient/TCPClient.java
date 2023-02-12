b7427fe42ff962938f6775af5a5946cf8a761a98
Keno <kenandi@kth.se>
2023-02-12 22:18:17 +0100

    Done

--- task2/tcpclient/TCPClient.java
+++ task2/tcpclient/TCPClient.java
@@ -46,7 +46,7 @@ public class TCPClient {
             InputStream inputStreamFromServer = clientSocket.getInputStream();
             int data = 0;
             int bytesRecieved = 0;
-            while (((data = inputStreamFromServer.read(fromServerBuffer)) != -1) && (limit == null || bytesRecieved + data <= limit)) {
+            while (((data = inputStreamFromServer.read(fromServerBuffer)) != -1) && (limit == null || bytesRecieved + data <= this.limit)) {
                 dataFromServer.write(fromServerBuffer, 0, data);
                 fromServerBuffer = new byte[BUFFERSIZE];
                 bytesRecieved += data;
