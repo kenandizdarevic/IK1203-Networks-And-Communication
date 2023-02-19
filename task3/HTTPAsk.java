4c053eaa62cd5592badf92384ca2bcb2d85b0f59
Keno <kenandi@kth.se>
2023-02-19 20:11:03 +0100

    Started task3

--- task3/HTTPAsk.java
+++ task3/HTTPAsk.java
@@ -2,8 +2,22 @@ import java.net.*;
 import java.io.*;
 
 public class HTTPAsk {
-    public static void main( String[] args) {
-        // Your code here
+    public static void main(String[] args) throws IOException{
+        try {
+            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+            while(true) {
+                // System.out.println("Testing");
+                Socket connectionSocket = serverSocket.accept();
+                // Read request from connectionSocekt
+                InputStream input = connectionSocket.getInputStream();
+                // Write reply to connectionSocket
+                OutputStream output = connectionSocket.getOutputStream();
+                // Close connectionSocket
+                System.out.println(output);
+            }
+        } catch(IOException exception) {
+            System.err.println(exception);
+        }
     }
 }
 
---
+++ task3/tcpclient/TCPClient.java
@@ -0,0 +1,80 @@
+package tcpclient;
+import java.net.*;
+
+import javax.naming.ldap.SortControl;
+
+import java.io.*;
+
+
+
+public class TCPClient {
+    public boolean shutdown; 
+    public Integer timeout;
+    public Integer limit;
+
+    public TCPClient(boolean shutdown, Integer timeout, Integer limit) {
+        this.shutdown = shutdown;
+        this.timeout = timeout;
+        this.limit = limit;
+    }
+
+    private static int BUFFERSIZE = 1024;
+
+    /**
+     * Establishes a connection to a server, sends & recieves data
+     * @param hostname server to establish a connection with
+     * @param port which port to use for connection
+     * @param toServerBytes byte[] to send to the server
+     * @return byte[] with data recieved from server
+     * @throws IOException
+     */
+    public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException, SocketTimeoutException {
+        
+        byte[] fromServerBuffer = new byte[BUFFERSIZE];
+        ByteArrayOutputStream dataFromServer = new ByteArrayOutputStream();      
+        
+        try (Socket clientSocket = new Socket(hostname, port)) {
+            clientSocket.getOutputStream().write(toServerBytes);
+            if (shutdown) {
+                clientSocket.shutdownOutput();
+            }
+
+            if (timeout != null) {
+                clientSocket.setSoTimeout(timeout);
+            }
+
+            InputStream inputStreamFromServer = clientSocket.getInputStream();
+            int data = 0;
+            int bytesRecieved = 0;
+            while ((data = inputStreamFromServer.read(fromServerBuffer)) != -1) {
+                if (limit != null && bytesRecieved + data > this.limit) {
+                    dataFromServer.write(fromServerBuffer, 0, this.limit - bytesRecieved);
+                    break;
+                }
+
+                dataFromServer.write(fromServerBuffer, 0, data);
+                fromServerBuffer = new byte[BUFFERSIZE];
+                bytesRecieved += data;
+            }
+            clientSocket.close();
+        }
+        catch (SocketTimeoutException exception) {
+            return dataFromServer.toByteArray();
+        }
+        catch (IOException exception) {
+            throw new IOException("Something went wrong: ", exception);
+        }
+        return dataFromServer.toByteArray();
+    }
+    
+   /**
+    * Helper function for askServer()
+    * @param hostname server to establish a connection with
+    * @param port which port to use for connection
+    * @return byte[] with data recieved from server
+    * @throws IOException
+    */
+    public byte[] askServer(String hostname, int port) throws IOException {
+        return askServer(hostname, port, null);
+    }
+}
